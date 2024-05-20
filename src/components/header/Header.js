c81ba81906ac6b13ec52db482d6c61079178bddb
Kavyapriya-1804 <kavya2002erd@gmail.com>
2024-05-20 19:13:32 +0530

    Fix: Added Header logo's navigation

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,5 +1,6 @@
 import "./Header.scss";
 import { PAGE_CONSTANTS } from "../../constants/pageConstants";
+import { ROUTE_CONSTANTS } from "../../constants/routeConstants";
 import logo from "../../assets/logos/crewMate_logo.png";
 import woman from "../../assets/icons/woman.png";
 import man from "../../assets/icons/man.png";
@@ -8,12 +9,12 @@ var {LOGO} = PAGE_CONSTANTS;
 
 const Header = () => (
   <header className="d-flex justify-content-between align-items-center">
-    <div className="brand d-flex align-items-center">
+    <a className="brand d-flex align-items-center text-decoration-none" href={ROUTE_CONSTANTS.HOME}>
       <div className="logo-wrapper">
         <img src={logo} alt="CrewMate" className="logo w-100" />
       </div>
       <h1 className="brand-name text-white mb-0">{LOGO.brandName}</h1>
-    </div>
+    </a>
     <div className="user-wrapper">
       <img src={woman} alt="User" className="user w-100" />
     </div>
